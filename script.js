6509df3b4ead3c398f7a88a73ff7b94c443054c3
kvetena <kveten.it@gmail.com>
2022-05-20 23:45:14 +0400

    play button added, computerPlay defined, computerSelection and playerSelection defined

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("linked succesfully")
+function computerPlay() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    let computerChoice = choices[Math.floor(Math.random()*choices.length)];
+    return computerChoice
+}
+
+const computerSelection = computerPlay();
+
+
+
+function promptPlayer(){
+    let playerSelection = prompt("Rock, Paper or Scissors?").toString().toUpperCase();
+    return playerSelection;
+}
+
+
