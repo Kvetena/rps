2104723efa8df28142754cf55466ccd194bda6f7
kvetena <kveten.it@gmail.com>
2022-05-31 19:05:32 +0400

    UI added, assignment changes are made

--- script.js
+++ script.js
@@ -7,42 +7,80 @@ function promptPlayer(){
     let playerChoice = prompt("Rock, Paper or Scissors?").toString().toUpperCase();
     return playerChoice;
 }
+let plScore = 0;
+let cpuScore = 0;
 
-function playRound(){
+let plText = document.querySelector('.plScore');
+let cpuText = document.querySelector('.cpuScore');
+
+function playRound(plChoice){
     const computerSelection = computerPlay();
-    const playerSelection = promptPlayer();
+    const playerSelection = plChoice;
+    
 
     if (computerSelection === "ROCK" & playerSelection === "ROCK") {
-        text.innerHTML = "You both chose Rock! It's a draw!, Play Again."
+        text.textContent = "You both chose Rock! It's a draw!, Play Again."
     } else if (computerSelection === "PAPER" & playerSelection === "PAPER") {
-        text.innerHTML = "You both chose Paper! It's a draw!, Play Again."
+        text.textContent = "You both chose Paper! It's a draw!, Play Again."
     } else if (computerSelection === "SCISSORS" & playerSelection === "SCISSORS") {
-        text.innerHTML = "You both chose Scissors! It's a draw!, Play Again."
+        text.textContent = "You both chose Scissors! It's a draw!, Play Again."
     } else if (computerSelection === "ROCK" & playerSelection === "PAPER") {
-        text.innerHTML = "Computer chose Rock, You chose Paper. You Won!, Play Again."
+        text.textContent = "Computer chose Rock, You chose Paper. You Won!, Play Again.";
+        ++plScore;
+        console.log("PL Score is " + plScore);
     } else if (computerSelection === "ROCK" & playerSelection === "SCISSORS") {
-        text.innerHTML = "Computer chose Rock, You chose Scissors. You Lost :/, Play Again."
+        text.textContent = "Computer chose Rock, You chose Scissors. You Lost :/, Play Again.";
+        ++cpuScore;
+        console.log("CPU Score is " + cpuScore);
     } else if (computerSelection === "PAPER" & playerSelection === "SCISSORS") {
-        text.innerHTML = "Computer chose Paper, You chose Scissors. You Won!, Play Again."
+        text.textContent = "Computer chose Paper, You chose Scissors. You Won!, Play Again."
+        ++plScore;
+        console.log("PL Score is " + plScore);
     } else if (computerSelection === "PAPER" & playerSelection === "ROCK") {
-        text.innerHTML = "Computer chose Paper, You chose Rock. You Lost :/, Play Again."
+        text.textContent = "Computer chose Paper, You chose Rock. You Lost :/, Play Again."
+        ++cpuScore;
+        console.log("CPU Score is " + cpuScore);
     } else if (computerSelection === "SCISSORS" & playerSelection === "ROCK") {
-        text.innerHTML = "Computer chose Scissors, You chose Rock. You Won!, Play Again."
+        text.textContent = "Computer chose Scissors, You chose Rock. You Won!, Play Again."
+        ++plScore;
+        console.log("PL Score is " + plScore);
     } else if (computerSelection === "SCISSORS" & playerSelection === "PAPER") {
-        text.innerHTML = "Computer chose Scissors, You chose Paper. You Lost :/, Play Again."
+        text.textContent = "Computer chose Scissors, You chose Paper. You Lost :/, Play Again."
+        ++cpuScore;
+        console.log("CPU Score is " + cpuScore);
     } else {
-        text.innerHTML = "You have not chosen, choose to play!"
+        text.textContent = "You have not chosen, choose to play!"
+    }
+
+    if (plScore < 5 & cpuScore < 5){
+        plText.textContent = plScore.toString();
+        cpuText.textContent = cpuScore.toString();
+    }else if (plScore >= 5) {
+        plText.textContent = plScore.toString();
+        cpuText.textContent = cpuScore.toString();
+        plScore = 0;
+        cpuScore = 0;
+        text.textContent = "You won best in 5! Play Again!"
+    }else if (cpuScore >= 5){
+        plText.textContent = plScore.toString();
+        cpuText.textContent = cpuScore.toString();
+        plScore = 0;
+        cpuScore = 0;
+        text.textContent = "You Lost :/ CPU won best in 5! Play Again!"
     }
     
 }
 
 var text = document.querySelector('.resultText');
 
-var button = document.querySelector('.wanna');
-button.addEventListener('click', playRound);
+let buttonChoices = document.querySelectorAll('#btnChoice');
 
-
-
+Array.from(buttonChoices).forEach(button => {
+    button.addEventListener("click", function() {
+      let plChoice = button.className.toString().toUpperCase();
+      playRound(plChoice);
+    });
+  });
 
 
 
